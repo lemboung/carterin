62f6e4106f76d605e570647daf4c4970178b65de
rifqir32 <rifqir32@gmail.com>
2016-09-18 21:10:07 +0700

    ubah FAQ
    FAQ fix

--- application/views/bantuan.php
+++ application/views/bantuan.php
@@ -18,65 +18,51 @@
                     <h1>Frequently Asked Questions</h1>
                 </div>
                 <div class="span6">
-                
+
                 </div>
             </div>
         </div>
     </section>
-    <!-- / .title -->       
+    <!-- / .title -->
 
     <section id="faqs" class="container">
         <ul class="faq">
          <li>
             <span class="number">01</span>
             <div>
-                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                <h4>apakah pihak carterin mendapatkan fee dari setiap trasaksi persewaan ?</h4>
+                <p>tidak, pihak carterin tidak mendapatkan fee sedikitpun dari setiap transaksi member </p>
             </div>
         </li>
 
         <li>
             <span class="number">02</span>
             <div>
-                <h4>consectetuer ipsum dolor sit amet</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi</p>
+                <h4>siapa yang bertanggung jawab jika terjadi penyalahagunaan oleh penyewa kendaraan ?</h4>
+                <p> jika terjadi penyalahgunaan oleh penyewa maka yang bertanggung jawab sepenuhnya adalah pihak yang menyewakan. </p>
             </div>
         </li>
 
         <li>
             <span class="number">03</span>
             <div>
-                <h4>adipiscing ipsum dolor sit amet, consectetuer</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis.</p>
+                <h4>bagaimana cara mengamankan mobil yang disewakan ?</h4>
+                <p>carterin menyediakan penyewaan gps dengan harga terjangkau bagi member.jika berminat silakan hubungi kami di nomor yang ada pada halaman about us </p>
             </div>
         </li>
 
         <li>
             <span class="number">04</span>
             <div>
-                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                <h4>apakah isi dari halaman blog dapat dipertanggung jawabkan ?</h4>
+                <p>setiap hari kami selalu memantau blog yang kami kelola.kami menyertakan sumber yang jelas untuk setiap postingan blog yang kami buat </p>
             </div>
         </li>
 
-        <li>
-            <span class="number">05</span>
-            <div>
-                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-            </div>
-        </li>
-
-        <li>
-            <span class="number">06</span>
-            <div>
-                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-            </div>
-        </li>
+        
     </ul>
     <p>&nbsp;</p>
-    
+
 </section>
 
 <!--Bottom-->
